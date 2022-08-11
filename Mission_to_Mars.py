52b33a3bee065397d230a90377016f187fc99a66
Clare Robbins <clarerobbins@Clares-MacBook-Pro.local>
2022-08-10 20:51:22 -0500

    Inttegrated MongoDB in app.py

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -11,54 +11,70 @@ import pandas as pd
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+def mars_news(browser):
 
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-slide_elem.find('div', class_='content_title')
+    #convert the browser to html to soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    
+    #add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
-
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    return news_title, news_p
 
 
 # ### Featured Images
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+def featured_image(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    #try/except for errors
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    except AttributeError:
+        return None
 
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    # Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    
+    return img_url
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
-
-df.to_html()
+def mars_facts():
+    try:
+        #use "read_html" to scrape the facts table into a datafram
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    except BaseException:
+        return None
+    
+    #assign columns and set index to df
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
+   
+    #convert df to html format, add bootstrap
+    return df.to_html()
 
 
 browser.quit()
--- app.py
+++ app.py
@@ -2,25 +2,24 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
-#Use flask_pymongo to set up mongo connection
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-# define home page
-@app.route('/')
+@app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template('index.html', mars=mars)
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
 # define next route
-@app.route('/scrape')
-def scrape(): 
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update_one({}, {'$set': mars_data}, upsert=True)
-    return redirect('/', code=302)
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
 
 if __name__ == '__main__':
     app.run()
