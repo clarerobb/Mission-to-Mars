9d2acb2f5d26f614b6360dc149bce9422550a9c0
Clare Robbins <clarerobbins@Clares-MacBook-Pro.local>
2022-08-12 20:32:35 -0500

    update

--- scraping.py
+++ scraping.py
@@ -22,6 +22,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        'mars_hemispheres': hemisphere_image_urls(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -99,6 +100,35 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemisphere_image_urls(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # parse html with soup
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+
+    # loop through each hemisphere
+    for hemi in range(4):
+        #create empty dictionary
+        hemispheres = {}        
+        #click image link
+        browser.find_by_css('a.product-item h3')[hemi].click()
+        #scrape image
+        element = browser.links.find_by_text('Sample').first
+        img_url = element['href']
+        #scrape title
+        title = browser.find_by_css("h2.title").text
+        #append image url and title to list
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
